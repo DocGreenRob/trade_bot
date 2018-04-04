4b8bfafe0fb0e034fd4f63b1fa5279da31dacbb1
Green <RGreen@esd.wa.gov>
2018-04-04 10:07:25 -0700

    just adding WebApi 2 project, need to add TDD

--- logic.cs
+++ logic.cs
@@ -1,23 +1,24 @@
-public class Position{
-    public int PositionId {get; set;}
-    public double UnderlyingPriceAtEntry {get; set;}
-    public IPositionType PositionType {get; set;}
-    public DateTime EntryTime {get; set;}
+using System.Collections.Generic;
+
+public class ProfitLoss{
+    public double PercentOpen {get; set;}
+    public double Open {get; set;}
+    public double OpenDay {get; set;}
 }
 
-public interface IPositionType{
+public interface IPosition{
     // dsggsd
 }
 
-public class Stock : IPositionType{
+public class Stock : IPosition{
     public 
 }
 
-public class Option : IPositionType{
+public class Option : IPosition{
     
 }
 
-public enum OptionPositionType{
+public enum OptionType{
     Naked,
     Stradle,
     Vertical
@@ -34,4 +35,26 @@ public void ManageProfit(){
 
 public bool IsReversal(int sessionInterval){
     // dfgsdg
+}
+
+public class Position{
+    public int PositionId {get; set;}
+    public double UnderlyingPriceAtEntry {get; set;}
+    public IPositionType PositionType {get; set;}
+    public DateTime EntryTime {get; set;}
+    public double ProfitLossOpen
+}
+
+public class Asset{
+    public List<Position> Positions {get; set;}
+}
+
+public void Main(){
+    Asset asset = new Asset();
+    asset.Positions = new List<Position>();
+    asset.Positions.Add(new Position{
+        PositionId = 1,
+        UnderlyingPriceAtEntry = 200,
+        PositionType = Option
+    });
 }
